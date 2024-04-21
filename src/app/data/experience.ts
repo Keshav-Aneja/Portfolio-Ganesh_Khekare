4da1befd1577e74a8438331a1b26db877ca860c4
Keshav-Aneja <keshav.aneja09@gmail.com>
2024-04-21 22:25:33 +0530

    feat: added data

--- src/app/data/experience.ts
+++ src/app/data/experience.ts
@@ -7,10 +7,32 @@ export const experienceData = [
     endDate: "21 April 2026",
   },
   {
-    name: "Vellore Institute of Technology(NIRF Rank 08)",
-    position: "Sr. Gr. Assistant Professor",
-    startDate: "14 Dec 2022",
+    name: "Parul University, Vadodara, Gujarat",
+    position: "HoD & Associate Professor",
+    startDate: "24 May 2021",
     currentlyWorking: false,
-    endDate: "21 April 2026",
+    endDate: "9 Dec 2022",
+  },
+
+  {
+    name: "GHRCE,(Autonomous)Nagpur",
+    position: "Assistant Professor",
+    startDate: "12 June 2012",
+    currentlyWorking: false,
+    endDate: "20 May 2021",
+  },
+  {
+    name: "PIET,Nagpur",
+    position: "Assistant Professor",
+    startDate: "24 Nov 2011",
+    currentlyWorking: false,
+    endDate: "15 March 2012",
+  },
+  {
+    name: "JIT,Nagpur",
+    position: "Lecturer",
+    startDate: "1 Sep 2010",
+    currentlyWorking: false,
+    endDate: "30 June 2011",
   },
 ];
